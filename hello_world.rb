7776a84b63818db5514df043f3d310bd09c3be8b
jcolago <jcolago2005@gmail.com>
2024-06-17 12:17:19 -0500

    Initial commit

---
+++ hello_world.rb
@@ -0,0 +1,3 @@
+puts 'Hello, World!'
+
+puts "It's Ruby time!"
---
+++ mad_libs.rb
@@ -0,0 +1,22 @@
+# Ruby Mad Libs Game
+
+# prompt user for different types of works
+puts 'Welcome to Mad Libs!'
+print 'Enter a noun:'
+noun = gets.chomp
+
+print 'Enter a verb:'
+verb = gets.chomp
+
+print 'Enter an adjective:'
+adjective = gets.chomp
+
+print 'Enter an adverb:'
+adverb = gets.chomp
+
+# use interpolation to create the story
+story = "The #{adjective} #{noun} decided to #{verb} #{adverb}."
+
+# display the story
+puts 'Here is your Mad Lib:'
+puts story
